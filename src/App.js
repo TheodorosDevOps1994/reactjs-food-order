fc599f8767b203665a9f90066cd4099c3417842e
TheodorosDevOps1994 <teokalmanidis@outlook.com>
2025-04-05 05:55:09 +0300

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
+
 
 function App() {
   return (
